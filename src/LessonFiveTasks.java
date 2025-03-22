d9c06c68998c320222006ba36ce7b2ddc6ac0910
Марина Деречина <18-9292@mail.ru>
2025-03-22 21:17:20 +0300

    added methods for lesson 5

---
+++ src/LessonFiveTasks.java
@@ -0,0 +1,43 @@
+package src;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LessonFiveTasks {
+    private static final int CAPACITY = 1;
+    public static void main(String[] args) {
+      int[] firstArray =   initArray(10);
+        addElement(firstArray, 7);
+    }
+
+    public static int[] initArray(int size) {
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(10);
+        }
+        System.out.println(Arrays.toString(array));
+
+        return array;
+    }
+
+    public static int[] addElement(int[] array,int newElement) {
+        int[] newArray = increaseSize(array);
+
+        newArray[newArray.length-1] = newElement;
+
+        System.out.println(Arrays.toString(newArray));
+
+        return newArray;
+    }
+
+    public static int[] increaseSize (int[] array){
+        int[] newArray = new int[array.length + CAPACITY];
+        newArray = Arrays.copyOf(array, newArray.length);
+
+        return newArray;
+    }
+
+
+}
