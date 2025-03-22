aac976dd8601f031dc7a2a27d6fae1c4ad5d41ac
Марина Деречина <18-9292@mail.ru>
2025-03-22 21:38:44 +0300

    added methods for lesson 5 part 2

--- src/LessonFiveTasks.java
+++ src/LessonFiveTasks.java
@@ -5,11 +5,18 @@ import java.util.Random;
 
 public class LessonFiveTasks {
     private static final int CAPACITY = 1;
+
     public static void main(String[] args) {
-      int[] firstArray =   initArray(10);
-        addElement(firstArray, 7);
+        int[] firstArray = initArray(10);
+        int[] secondArray = addElement(firstArray, 7);
+        getElementByIndex(firstArray, 6);
+        getArraySize(firstArray);
+        getArraySize(secondArray);
     }
 
+    /**
+     * Task #1
+     */
     public static int[] initArray(int size) {
         Random random = new Random();
         int[] array = new int[size];
@@ -22,21 +29,31 @@ public class LessonFiveTasks {
         return array;
     }
 
-    public static int[] addElement(int[] array,int newElement) {
+    public static int[] addElement(int[] array, int newElement) {
         int[] newArray = increaseSize(array);
 
-        newArray[newArray.length-1] = newElement;
+        newArray[newArray.length - 1] = newElement;
 
         System.out.println(Arrays.toString(newArray));
 
         return newArray;
     }
 
-    public static int[] increaseSize (int[] array){
+    public static int[] increaseSize(int[] array) {
         int[] newArray = new int[array.length + CAPACITY];
         newArray = Arrays.copyOf(array, newArray.length);
 
         return newArray;
+    }
+
+    public static int getElementByIndex(int[] array, int index) {
+        System.out.println(array[index]);
+        return array[index];
+    }
+
+    public static int getArraySize(int[] array) {
+        System.out.println("array size: " + array.length);
+        return array.length;
     }
 
 
